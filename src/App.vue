bfe20321c18e7b6c6479190e663ad83f5a6bb4d5
Oleg Spiridonov <spiridonov.oa@gmail.com>
2021-02-04 02:43:11 -0800

    Style heart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeartCard :msg="msg[0]" />
+    <div class="heart-img">
+      <img alt="Vue logo" color="#333" src="./assets/heart-black.svg" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeartCard from "./components/HeartCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeartCard,
+  },
+  data: () => {
+    return {
+      msg: ["You are the best person I know"],
+    };
+  },
+};
 </script>
 
 <style>
+body,
+html {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d31c99;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  background: #ff7ed6;
+}
+
+.heart-img {
+  width: 300px;
+  filter: invert(19%) sepia(96%) saturate(5450%) hue-rotate(308deg) brightness(92%) contrast(86%);
 }
 </style>
---
+++ src/components/HeartCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="heart">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeartCard",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
