2205a6fab9fbb7d2ab9dd39f0ab407f30a056042
Oleg Spiridonov <spiridonov.oa@gmail.com>
2021-02-07 02:08:33 -0800

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
-import { routes } from "./router";
+import vuetify from "./plugins/vuetify";
+import router from "./plugins/router";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueRouter);
-const router = new VueRouter({ routes, mode: "history" });
-
 new Vue({
   render: (h) => h(App),
+  vuetify,
   router,
 }).$mount("#app");
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,6 +1,11 @@
 import NavLinks from "./components/NavLinks.vue";
 import HeartCard from "./components/HeartCard.vue";
 
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
 export const routes = [
   {
     path: "/",
@@ -11,3 +16,5 @@ export const routes = [
     component: HeartCard,
   },
 ];
+
+export default new VueRouter({ routes, mode: "history" });
