683513952774132485c586fa6650bcee6ce641d0
Oleg Spiridonov <spiridonov.oa@gmail.com>
2021-02-06 19:39:55 -0800

    Refactorign main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <div class="content">
-      <HeartCard :msg="msg[0]" />
-      <div class="heart-img">
-        <img alt="Vue logo" color="#333" src="./assets/heart-black.svg" />
-      </div>
-    </div>
+    <HeartCard :msg="msg[0]" />
   </div>
 </template>
 
@@ -19,7 +14,7 @@ export default {
   },
   data: () => {
     return {
-      msg: ["You are the only one I have"],
+      msg: ["I love you baby"],
     };
   },
 };
@@ -32,63 +27,16 @@ html {
   margin: 0;
 }
 
-@keyframes showAnimation {
-  0% {
-    opacity: 0;
-  }
-  10% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
-@keyframes bounce-4 {
-  0% {
-    transform: scale(1, 1) translateY(0);
-  }
-  15% {
-    transform: scale(1.1, 1.1) translateY(0);
-  }
-  50% {
-    transform: scale(1, 1) translateY(0);
-  }
-  100% {
-    transform: scale(1, 1) translateY(0);
-  }
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #d31c99;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 100%;
   height: 100%;
-  background: #ff7ed6;
-}
-
-.content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  animation-duration: 2s;
-  animation-name: showAnimation;
-  animation-timing-function: ease;
-}
-
-.heart-img {
-  animation-duration: 1.5s;
-  animation-name: bounce-4;
-  animation-timing-function: ease;
-  animation-iteration-count: infinite;
-  width: 300px;
-  filter: invert(62%) sepia(81%) saturate(7499%) hue-rotate(310deg) brightness(100%) contrast(98%);
 }
 </style>
--- src/components/HeartCard.vue
+++ src/components/HeartCard.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="heart">
-    <h1>{{ msg }}</h1>
+  <div class="heart__bg">
+    <div class="heart__content">
+      <h1 class="heart__title">{{ msg }}</h1>
+      <div class="heart__img">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 122.88 107.41"
+          xml:space="preserve"
+        >
+          <g>
+            <path
+              class="st0"
+              d="M60.83,17.19C68.84,8.84,74.45,1.62,86.79,0.21c23.17-2.66,44.48,21.06,32.78,44.41 c-3.33,6.65-10.11,14.56-17.61,22.32c-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.56C29.16,76.9,0.95,55.93,0.02,29.95 C-0.63,11.75,13.73,0.09,30.25,0.3C45.01,0.5,51.22,7.84,60.83,17.19L60.83,17.19L60.83,17.19z"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,9 +36,72 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
+.heart__bg {
+  flex: 1;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ff7ed6;
+}
+
+.heart__title {
   margin: 0;
   padding: 1em;
   font-size: 24px;
+  color: #d31c99;
+}
+
+.heart__img svg {
+  fill: #d31c99;
+}
+
+.heart__content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  animation-duration: 2s;
+  animation-name: showAnimation;
+  animation-timing-function: ease;
+}
+
+.heart__img {
+  animation-duration: 1s;
+  animation-name: bounce-4;
+  animation-timing-function: ease;
+  animation-iteration-count: infinite;
+  width: 250px;
+}
+
+@keyframes showAnimation {
+  0% {
+    opacity: 0;
+  }
+  10% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes bounce-4 {
+  0% {
+    transform: scale(1, 1) translateY(0);
+  }
+  15% {
+    transform: scale(1.1, 1.1) translateY(0);
+  }
+  50% {
+    transform: scale(1, 1) translateY(0);
+  }
+  65% {
+    transform: scale(1.02, 1.02) translateY(0);
+  }
+  100% {
+    transform: scale(1, 1) translateY(0);
+  }
 }
 </style>
